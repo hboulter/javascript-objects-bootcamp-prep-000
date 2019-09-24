4de670d9451a990a3d44e2d5e3b9fbcb5bcd04ff
Heidi B <boulter.heidi@gmail.com>
2019-09-24 19:43:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,5 @@ return Object.assign(playlist, { [artistName] : songTitle })
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  playlist;
+  delete playlist[artistName];
 }
